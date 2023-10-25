0ed3a7c6cba47e55edf071ce164d2ad5313a076f
Ivan Seleznov <ivan14.02434@gmail.com>
2023-10-25 23:54:36 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppLab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var length = Convert.ToDouble(LengthTextBox.Text);
+                var width = Convert.ToDouble(WidthTextBox.Text);
+
+                Rectangle Rectangle = new Rectangle(length, width);
+
+                PerimeterText.Content = Rectangle.CalculatePerimeter();
+                AreaText.Content = Rectangle.CalculateArea();
+            } 
+            catch
+            {
+                MessageBox.Show("Введено невірні дані.");
+            }
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppLab1
+{
+    internal class Rectangle
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+
+        public double CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+}
